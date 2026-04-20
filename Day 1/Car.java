18960edcf5405e38024e3c5cc23766da39591b28
RiNa-036 <it24036@mbstu.ac.bd>
2026-04-20 08:27:00 +0600

    Add Car class with attributes and display method

---
+++ Day 1/Car.java
@@ -0,0 +1,12 @@
+Public class Car{
+ //Attributes of the Car class
+ String color;
+ String model;
+ int year;
+ // Method to display car information
+ Void displayInfo(){
+  System.out.println("Car Model: " + model);
+  System.out.println("Car Color: " + color);
+  System.out.println("Car Year: " + year);
+ }
+}
