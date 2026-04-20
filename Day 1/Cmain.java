5797a6a915a4a93374cb9cdba9c548fbb26ca136
RiNa-036 <it24036@mbstu.ac.bd>
2026-04-20 08:28:37 +0600

    Add Main class

---
+++ Day 1/Cmain.java
@@ -0,0 +1,12 @@
+public class Main {
+ public static void main(String[] args){
+  //Creating an object of the Car class
+  Car myCar = new Car();
+  //Assigning values to the object's attributes
+  myCar.color = "Red";
+  myCar.model = "Toyota";
+  myCar.year = 2020;
+  // Calling the displayInfo method
+  myCar.displayInfo();
+ }
+}
