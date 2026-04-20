a700c761cec19d53fc971724ab6f510f3e627d65
RiNa-036 <it24036@mbstu.ac.bd>
2026-04-20 08:31:11 +0600

    Add Main class

---
+++ Day 1/Stmain.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student("Rina", 21, 3.8);
+        Student s2 = new Student("Shorna", 21, 3.9);
+
+        s1.displayInfo();
+        System.out.println("---");
+        s2.displayInfo();
+    }
+}
